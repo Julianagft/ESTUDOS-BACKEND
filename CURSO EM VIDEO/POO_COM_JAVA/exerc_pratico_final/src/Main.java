82fd297c70c756cf8901135dca9f05f3980f46f5
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-06-26 20:49:11 -0300

    poo ultima aula

--- CURSO EM VIDEO/POO_COM_JAVA/exerc_pratico_final/src/Main.java
+++ CURSO EM VIDEO/POO_COM_JAVA/exerc_pratico_final/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         v[1] = new Video("Aula 12 de PHP");
         v[2] = new Video("Aula 10 de HTML5");
 
-        System.out.println(v[0].toString());
+//        System.out.println(v[0].toString());
 
         Gafanhoto g[] = new Gafanhoto[3];
 
@@ -13,7 +13,14 @@ public class Main {
         g[1] = new Gafanhoto("Jubilete", 23, "F", "juju_gatinha");
         g[2] = new Gafanhoto("Thor", 15, "F", "rei_do_trovão");
 
-        System.out.println(g[0].toString());
+//        System.out.println(g[0].toString());
+
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]);
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0], v[1]);
+        System.out.println(vis[1].toString());
 
     }
 }
