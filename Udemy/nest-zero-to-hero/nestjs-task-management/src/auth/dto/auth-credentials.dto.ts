c73efa5e753094a4ee36614931c1c128a9be9092
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-10-16 22:08:30 -0300

    creando usuario para autenticação

---
+++ Udemy/nest-zero-to-hero/nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+
+export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    username: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: "The password should contain at least 8 characters, including one uppercase letter, one lowercase letter, one special character, and one number."
+    }) // regex de validação de senha.
+    password: string;
+}
+
