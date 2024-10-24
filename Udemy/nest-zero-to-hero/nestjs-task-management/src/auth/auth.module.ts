573dc6982466f7aff8c8d5c4feabdc846a413e5c
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-10-23 22:11:08 -0300

    autenticação com jwt

--- Udemy/nest-zero-to-hero/nestjs-task-management/src/auth/auth.module.ts
+++ Udemy/nest-zero-to-hero/nestjs-task-management/src/auth/auth.module.ts
@@ -4,9 +4,19 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersRepository } from './user.repository';
+import { DefaultNamingStrategy } from 'typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ DefaultNamingStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600, //define o tempo de expiração do token;
+      },
+    }),
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
