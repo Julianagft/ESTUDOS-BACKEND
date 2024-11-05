5f6a628b4cd247f491ffbc0ca3980bb2f4211ae2
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-11-04 22:21:26 -0300

    aula 74

--- Udemy/nest-zero-to-hero/nestjs-task-management/src/main.ts
+++ Udemy/nest-zero-to-hero/nestjs-task-management/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; //basicamente vai fazer o trabalho de validação por nós 
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  
   await app.listen(3001);
 }
 
