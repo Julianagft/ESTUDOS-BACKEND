58571e01ddae365e2b6fc9d3ae96f36ae9d3b9d9
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2024-11-11 21:57:47 -0300

    aula 89 finalizada

--- Udemy/nest-zero-to-hero/nestjs-task-management/src/app.module.ts
+++ Udemy/nest-zero-to-hero/nestjs-task-management/src/app.module.ts
@@ -1,26 +1,30 @@
   import { Module } from '@nestjs/common';
-  import { ConfigModule } from '@nestjs/config';
+  import { ConfigModule, ConfigService } from '@nestjs/config';
   import { TasksModule } from './tasks/tasks.module';
   import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
+  import { AuthModule } from './auth/auth.module';
+  import { configValidationSchema } from 'config.schema';
 
   @Module({
     imports: [
       ConfigModule.forRoot({ 
         envFilePath: `.env.stage.${process.env.STAGE}`,
-        isGlobal: true,
+        validationSchema: configValidationSchema,
       }),
       TasksModule,
-      TypeOrmModule.forRoot({
-        //em partes globais usamos o forRoot, e em submodulos usamos o forFeature;
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Lobinho1!',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        // useFactory é uma função que será inicializada pelo Nest.js sempre que quisermos inicializar esse módulo.
+        useFactory: async (configService: ConfigService) => ({
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+        }),
       }),
       AuthModule,
     ],
