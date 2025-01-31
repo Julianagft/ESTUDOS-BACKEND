a1f8c2a2f1d6053548647b7b090a1f54c2dbbea0
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-01-30 22:15:03 -0300

    aprendendo a organizar a aplicação

---
+++ balta.io/fundamento-c#/MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyApp.Test; 
+// ("Test" é o nome do arquico que irei usar aqui dentro)
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ balta.io/fundamento-c#/MyApp/Test.cs
@@ -0,0 +1,8 @@
+namespace MyApp {
+    class Teste {
+
+    }
+}
+
+// Não pode ter o mesmo namespace E a mesma classe. Posso repetir o namespace (embora não seja recomendado), mas não a classe.  
+// Podemos ter um namespace dentro de outro usando o ponto ".". 
