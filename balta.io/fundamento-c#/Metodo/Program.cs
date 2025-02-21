2512855ea473f5acf6559ced20c8921b5b8460e7
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-02-20 22:06:02 -0300

    Structure pratica

---
+++ balta.io/fundamento-c#/Metodo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Operadores {
+    class Program {
+        static void Main(string[] args) {
+            MeuMetodo();
+        }
+        // Não posso ter método dentro de método dentro de C#
+
+        static void MeuMetodo() {
+            Console.WriteLine("Meu método");
+        }
+    }
+}
---
+++ balta.io/fundamento-c#/Structure/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(2, "Mouse Gamer", 299.97);
+            // tanto posso dev=finir o valor da variável na criação do objeto quanto depois;
+            mouse.Id = 1;
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            
+        }
+    }
+
+    struct Product 
+    {
+
+        // A única diferença é que quando criamos um método construtor precisamos explicitar as propriedades;
+        public Product(int Id, string Name, double Price) {
+            this.Id = Id;
+            this.Name = Name;
+            this.Price = Price;
+
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+        public double PriceInDolar(double dolar) {
+            return Price * dolar;
+        }
+    }
+}
