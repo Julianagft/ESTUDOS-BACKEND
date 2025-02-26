b0af6c7997622dbc79a9b09964dd20a08368b479
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-02-25 22:09:44 -0300

    calculadora concluida

---
+++ balta.io/fundamento-c#/Calculadora/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("0 - Sair");
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res) {
+                case 1:
+                    Console.WriteLine("Você escolheu Soma"); 
+                    Soma();
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu Subtração");
+                    Subtracao();
+                    break;
+                case 3:
+                    Console.WriteLine("Você escolheu Divisão");
+                    Divisao();
+                    break;
+                case 4:
+                    Console.WriteLine("Você escolheu Multiplicação");
+                    Multiplicacao();
+                    break;
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Soma() {
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A soma de {v1} e {v2} é {v1 + v2}");
+        }
+
+        static void Subtracao() {
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A subtração de {v1} e {v2} é {v1 - v2}");    
+        }
+
+        static void Divisao() {
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{v1} dividido por {v2} é {v1 / v2}");    
+        }
+
+        static void Multiplicacao() {
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{v1} multiplicado por {v2} é {v1 * v2}");    
+        }
+    }
+}
--- balta.io/fundamento-c#/Calculadora/obj/Debug/net8.0/Enum.AssemblyInfo.cs
+++ balta.io/fundamento-c#/Calculadora/obj/Debug/net8.0/Enum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2512855ea473f5acf6559ced20c8921b5b8460e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c10a0461194c0007599b388bf8b10752812396")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
