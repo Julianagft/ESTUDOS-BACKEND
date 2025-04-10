a7358bba6b11e777aca8687f05d02f5abc46d154
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-04-09 22:44:10 -0300

    finalizando o curso de fundamentos de c#

---
+++ balta.io/fundamento-ccharpe/Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Common;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var funcionarios = new Funcionario[1];
+            funcionarios[0] = new Funcionario() {id = 123};
+
+            foreach (var funcionario in funcionarios) {
+                Console.WriteLine(funcionario.id);
+            }
+        }
+
+        public struct Funcionario 
+        {
+            public int id { get; set; }
+           
+        }
+
+    }
+}
