917fdf66fdcae97604322fcad9af7d2c4671c088
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-04-23 22:24:57 -0300

    criando a conta

--- Udemy/curso_node_do_zero/4-PROJETO-1-ACOUNTS/src/index.js
+++ Udemy/curso_node_do_zero/4-PROJETO-1-ACOUNTS/src/index.js
@@ -13,8 +13,8 @@ async function operations() {
                 name: 'action',
                 message: 'O que você deseja fazer?',
                 choices: [
-                    'Criar conta',
-                    'Consultar saldo',
+                    'Criar Conta',
+                    'Consultar Saldo',
                     'Depositar',
                     'Sacar',
                     'Transferir',
@@ -24,9 +24,53 @@ async function operations() {
         ]);
 
         console.log(chalk.green(`Ação selecionada: ${action}`));
+
+        if (action === 'Criar Conta') {
+            createAccount();
+        }
     } catch (err) {
         console.error(chalk.red('Erro ao processar a operação:'), err);
     }
 }
+
+async function createAccount() {
+    try {   
+        console.log(chalk.green('Defina as opções da sua conta a seguir'));
+    
+        const { accountName } = await inquirer.prompt([
+            {
+                name: 'accountName',
+                message:'Digite um nome para sua conta:',
+            }
+        ]);
+
+        console.info(accountName);
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts');
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Essa conta já existe!'));
+            createAccount();
+            return;
+        }
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`,
+            JSON.stringify({ balance: 0, transactions: [] }, null, 2),
+            'utf-8'
+        );
+
+        console.log(chalk.bgGreen.black('Parabéns, sua conta foi criada com sucesso! Obrigada por escolher o nosso banco!'));
+
+        operations();
+
+    } catch (err) {
+        console.error(chalk.red('Erro ao criar conta:'), err);
+    }
+}
+
+
 
 operations();
