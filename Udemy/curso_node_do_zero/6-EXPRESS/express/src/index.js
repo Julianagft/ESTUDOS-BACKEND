316ae121d8f7eb5f7b240b6387e5b51f3d7e4df9
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-05-05 22:38:25 -0300

    middleware

--- Udemy/curso_node_do_zero/6-EXPRESS/express/src/index.js
+++ Udemy/curso_node_do_zero/6-EXPRESS/express/src/index.js
@@ -7,13 +7,59 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json());
+
 const basePath = path.join(__dirname, 'templates');
+
+// const checkAuth = function(req, res, next) {
+//     req.authStatus = true;
+
+//     if(req.authStatus) {
+//         console.log('Está logado, pode continuar');
+//         next();
+//     } else {
+//         console.log('Não está logado, faça o login para continuar');
+//         next();
+//     }
+// }
+
+// app.use(checkAuth);
+
+app.get('/users/add', (req, res) => {
+    res.sendFile(`${basePath}/userForm.html`);
+});
+
+app.post('/users/save', (req, res) => {
+    console.log(req.body);
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    console.log(`Estamos buscando pelo usuário ${id}`);
+
+    res.sendFile(`${basePath}/users.html`);
+
+});
 
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`);
 });
 
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
+
+
+
+
+// Aplicação monolito (back e front juntos);
 
