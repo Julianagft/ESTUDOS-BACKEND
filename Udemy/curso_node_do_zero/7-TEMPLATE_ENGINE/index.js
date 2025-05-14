f3d7f40cdd190e36799a694276b3866a7d58d559
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-05-14 15:28:17 -0300

    pra que handlebars se tem react?

--- Udemy/curso_node_do_zero/7-TEMPLATE_ENGINE/index.js
+++ Udemy/curso_node_do_zero/7-TEMPLATE_ENGINE/index.js
@@ -6,8 +6,24 @@ const app = express();
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
 app.get('/', (req, res) => {
-    res.render('home');
+    const user = {
+        name: "Juliana",
+        surname: "Campos",
+        age: 29,
+    };
+
+    const palavra = "Teste";
+
+    const auth = false;
+
+    const approved = true
+
+    res.render('home', { user: user, palavra, auth, approved });
 });
 
 app.listen(3000, () => {
