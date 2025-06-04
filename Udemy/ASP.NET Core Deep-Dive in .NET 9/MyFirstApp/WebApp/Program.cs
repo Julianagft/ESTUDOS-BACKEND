21be45e59e67b4b25b8b8b770e9b0f5f678fe526
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-06-03 22:24:32 -0300

    Método GET

--- Udemy/ASP.NET Core Deep-Dive in .NET 9/MyFirstApp/WebApp/Program.cs
+++ Udemy/ASP.NET Core Deep-Dive in .NET 9/MyFirstApp/WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,16 +7,63 @@ app.MapGet("/", () => "Hello World!");
 
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync($"The method is: {context.Request.Method}\r\n");
-    await context.Response.WriteAsync($"The Url is: {context.Request.Path}\r\n");
-
-    await context.Response.WriteAsync($"Headers");
-
-    foreach (var key in context.Response.Headers.Keys)
+    if (context.Request.Method == "GET")
     {
-        context.Response.WriteAsync($"{key}: {context.Response.Headers[key]}");
+        if (context.Request.Path.StartsWithSegments("/"))
+        {
+            await context.Response.WriteAsync($"The method is: {context.Request.Method}.\r\n");
+            await context.Response.WriteAsync($"The Url is: {context.Request.Path}.\r\n");
+            await context.Response.WriteAsync("\r\nHeaders:\r\n");
+
+            foreach (var key in context.Request.Headers.Keys)
+            {
+                await context.Response.WriteAsync($"{key}: {context.Request.Headers[key]}\r\n");
+            }
+        }
+        else if (context.Request.Path.StartsWithSegments("/employees"))
+        {
+            var employees = EmployeesRepository.GetAllEmployees();
+
+            foreach (var employee in employees)
+            {
+                await context.Response.WriteAsync($"{employee.Name}: {employee.Position}\r\n");
+            }
+        }
     }
+    
+   
+
+
 
 });
 
 app.Run();
+
+static class EmployeesRepository
+{
+    private static List<Employee> employees = new List<Employee>
+    {
+        new Employee(1, "John Doe", "Developer", 60000),
+        new Employee(2, "Bob", "Designer", 55000),
+        new Employee(3, "Charlie", "Manager", 70000)
+    };
+
+    public static List<Employee> GetAllEmployees() => employees;
+
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(int id, string name, string position, double salary)
+    {
+        Id = id;
+        Name = name;
+        Position = position;
+        Salary = salary;
+    }
+}
--- Udemy/ASP.NET Core Deep-Dive in .NET 9/MyFirstApp/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ Udemy/ASP.NET Core Deep-Dive in .NET 9/MyFirstApp/WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00d8fa030153b8de2e3d11ef473701bc6f27eba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7905f3349d47e779393ab68c24b318838530e80b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
