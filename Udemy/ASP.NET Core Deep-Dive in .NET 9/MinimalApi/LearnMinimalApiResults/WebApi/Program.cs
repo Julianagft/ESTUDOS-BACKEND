960f32ad63efed8bb001ff8fedba4923f66aee18
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-08-04 06:26:37 -0300

    minimal api construindo os metodos

---
+++ Udemy/ASP.NET Core Deep-Dive in .NET 9/MinimalApi/LearnMinimalApiResults/WebApi/Program.cs
@@ -0,0 +1,75 @@
+using System.Text.Json;
+using WebApi.Models;
+using WebApi.Results;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+
+app.UseStatusCodePages();
+
+app.MapGet("/", HtmlResult () =>
+{
+    string html = "<h2>Welcome to our API</h2> Our API is used to learn ASP.NET CORE.";
+
+    return new HtmlResult(html);
+});
+
+app.MapGet("/employees", () =>
+{
+    var employees = EmployeesRepository.GetEmployees();
+
+    return TypedResults.Ok(employees);
+});
+
+app.MapGet("/employees/{id:int}", (int id) =>
+{
+    var employee = EmployeesRepository.GetEmployeeById(id);
+
+    return employee is not null ? TypedResults.Ok(employee) 
+        : Results.ValidationProblem(new Dictionary<string, string[]>
+        {
+            {"id", new[] { $"Employee with the id {id} doesn't exist." } }
+        });
+});
+
+app.MapPost("/employees", (Employee employee) =>
+{
+    if (employee is null || employee.Id < 0)
+    {
+        return Results.ValidationProblem(new Dictionary<string, string[]>
+        {
+            {"id", new[] { "Employee is not provided or is not valid." } }
+        });
+    }
+
+    EmployeesRepository.AddEmployee(employee);
+    return TypedResults.Created($"/employees/{employee.Id}", employee);    
+
+}).WithParameterValidation();
+
+app.MapPut("/employees/{id:int}", (int id, Employee employee) =>
+{
+    if (id != employee.Id)
+    {
+        return Results.ValidationProblem(new Dictionary<string, string[]>
+        {
+            {"id", new[] {"Employee id is not the same as id."} }
+        });
+    }
+
+    return EmployeesRepository.UpdateEmployee(employee)?TypedResults.NoContent()
+    : Results.ValidationProblem(new Dictionary<string, string[]>
+    {
+        {"id", new[] { $"Employee doesn't exist." }   }
+    });
+});
+
+app.Run();
