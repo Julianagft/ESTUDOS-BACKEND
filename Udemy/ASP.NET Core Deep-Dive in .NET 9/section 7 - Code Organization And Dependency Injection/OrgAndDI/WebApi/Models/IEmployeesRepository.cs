ce16f4ea82f4ba6af2f4649592e5fe124f7132e8
Juliana Rodrigues <juliana.camposrodrigues7@gmail.com>
2025-08-05 11:50:47 -0300

    finalizando minimalApi e aprendendo sobre dependency injection

---
+++ Udemy/ASP.NET Core Deep-Dive in .NET 9/section 7 - Code Organization And Dependency Injection/OrgAndDI/WebApi/Models/IEmployeesRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebApi.Models
+{
+    public interface IEmployeesRepository
+    {
+        abstract void AddEmployee(Employee? employee);
+        abstract bool DeleteEmployee(Employee? employee);
+        abstract Employee? GetEmployeeById(int id);
+        abstract List<Employee> GetEmployees();
+        abstract bool UpdateEmployee(Employee? employee);
+    }
+}
